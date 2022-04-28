4977eda5867091a5a38d5238ea34fa6835133ee3
Mac Schmitz <mac.schmitz@att.net>
2022-04-27 22:46:33 -0400

    Added the README template

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# hw-9-readme-generator
+# Professional README Generator
 
-A command-line application to easily generate a README file
+## Description
+A command line program that easily generates a professional README.md file
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+No installation necessary.
+
+## Usage
+Run node index.js command in your terminal
+
+## License
+MIT License
+
+## How to Contribute
+undefined
+
+## Tests
+Test it out by running it in your terminal
+
+## Questions
+Visit my GitHub profile: mschmitzzz
+If you have any additional questions about this project, you can email me at mac@mac.me
--- index.js
+++ index.js
@@ -5,32 +5,38 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project?',
+        message: 'What is the name of your project?',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Please add a description of your project',
+        message: 'Write a description of your project.',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Please enter the installation instructions for this project',
+        message: 'How do you install the app?',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Please describe how this project is used.',
+        message: 'How do you use this app?',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'constribution',
         message: 'Please describe the contribution guidelines for this project.',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
-        name: 'testing',
+        name: 'test',
         message: 'Please describe instructions for testing this application.',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'list',
@@ -50,23 +56,58 @@ const questions = [
             'GNU Lesser General Public License v.2.1', 
             'Mozilla Public License 2.0', 
             'The Unlicense',
+            'None',
         ],
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username?',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
     {
         type: 'input',
         name: 'email',
         message: 'Please enter your email address.',
+        validate: (value) => {if(value){return true} else {return 'Enter a value to continue.'}}
     },
 ];
 
 inquirer.prompt(questions).then ((data) => {
-    let readmeCreate = `readme text ${data.title}`;
+    let readmeTemplate = 
+`# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## How to Contribute
+${data.contribution}
+
+## Tests
+${data.test}
+
+## Questions
+Visit my GitHub profile: ${data.github}
+If you have any additional questions about this project, you can email me at ${data.email}`;
+
     const fileName = `README.md`;
-    fs.writeFile(fileName, readmeCreate, (err) =>
+    fs.writeFile(fileName, readmeTemplate, (err) =>
     err ? console.log(err) : console.log('Success!'));
 });
